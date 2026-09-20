87fbaa89255ab2008c5570486c5a3c1621c110fe
skgdrive932 <skgdrive932@gmail.com>
2026-09-20 12:14:15 +0530

    Update AboutActivity.kt

--- app/src/main/java/com/example/prorearcam/AboutActivity.kt
+++ app/src/main/java/com/example/prorearcam/AboutActivity.kt
@@ -1,9 +1,10 @@
 package com.example.prorearcam
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prorearcam.R
 
-class AboutActivity : Activity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
